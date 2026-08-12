c819fd5aa33a6db1c8a5ddc720df9b441594224a
Brenno Pereira <brennuscontato@gmail.com>
2026-08-12 16:09:23 -0300

    initpy atualizado

--- observability-platform/middleware/__init__.py
+++ observability-platform/middleware/__init__.py
@@ -2,7 +2,12 @@ from .logging import (
     request_logging_middleware,
 )
 
+from .metrics import (
+    metrics_middleware,
+)
+
 
 __all__ = [
     "request_logging_middleware",
+    "metrics_middleware",
 ]
