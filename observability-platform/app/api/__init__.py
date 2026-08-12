fb4464b9fd477cb0dda6914420f66216046e5f47
Brenno Pereira <brennuscontato@gmail.com>
2026-08-12 14:37:45 -0300

    update init

--- observability-platform/app/api/__init__.py
+++ observability-platform/app/api/__init__.py
@@ -1,7 +1,18 @@
-from app.api.log_routes import router as log_router
-from app.api.routes import router
+from app.api.alert_routes import (
+    router as alert_router,
+)
+
+from app.api.log_routes import (
+    router as log_router,
+)
+
+from app.api.routes import (
+    router,
+)
+
 
 __all__ = [
     "router",
     "log_router",
+    "alert_router",
 ]
