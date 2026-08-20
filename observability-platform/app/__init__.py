ba9db54cbf1c012863d6da6717eb85dbc566139d
Brenno Pereira <brennuscontato@gmail.com>
2026-08-20 19:48:04 -0300

    Update init

---
+++ observability-platform/app/__init__.py
@@ -0,0 +1,28 @@
+from app.api.alert_routes import (
+    router as alert_router,
+)
+
+from app.api.application_metrics_routes import (
+    router as application_metrics_router,
+)
+
+from app.api.log_routes import (
+    router as log_router,
+)
+
+from app.api.routes import (
+    router,
+)
+
+from app.api.trace_routes import (
+    router as trace_router,
+)
+
+
+__all__ = [
+    "router",
+    "alert_router",
+    "application_metrics_router",
+    "log_router",
+    "trace_router",
+]
