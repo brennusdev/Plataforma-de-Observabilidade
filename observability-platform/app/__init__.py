287705b16bd9fb6c0d10560938fb5154d3ec921d
Brenno Pereira <brennuscontato@gmail.com>
2026-08-20 20:02:46 -0300

    Init py atualizado
    Versão de atualização v7

--- observability-platform/app/__init__.py
+++ observability-platform/app/__init__.py
@@ -6,6 +6,10 @@ from app.api.application_metrics_routes import (
     router as application_metrics_router,
 )
 
+from app.api.health_routes import (
+    router as health_router,
+)
+
 from app.api.log_routes import (
     router as log_router,
 )
@@ -23,6 +27,7 @@ __all__ = [
     "router",
     "alert_router",
     "application_metrics_router",
+    "health_router",
     "log_router",
     "trace_router",
 ]
