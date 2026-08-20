1a44b7af136cff5014dbb9a0f27e4c9fc34de545
Brenno Pereira <brennuscontato@gmail.com>
2026-08-20 20:03:13 -0300

    Update no collector
    Versão de atualização v7

--- observability-platform/app/observability/collector.py
+++ observability-platform/app/observability/collector.py
@@ -1,6 +1,7 @@
 import psutil
 
 from app.observability.prometheus import (
+    APPLICATION_INFO,
     APPLICATION_UP,
     CPU_USAGE,
     DISK_USAGE,
@@ -23,5 +24,11 @@ def collect_system_metrics():
     DISK_USAGE.set(
         psutil.disk_usage("/").percent
     )
+
+    APPLICATION_INFO.labels(
+        service="observability-api",
+        version="7.0.0",
+        environment="development",
+    ).set(1)
 
     APPLICATION_UP.set(1)
