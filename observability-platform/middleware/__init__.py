7ca7c6facd49e34c0a5afcdd2a47f9a0acd820a4
Brenno Pereira <brennuscontato@gmail.com>
2026-08-20 19:44:28 -0300

    update no init

--- observability-platform/middleware/__init__.py
+++ observability-platform/middleware/__init__.py
@@ -6,8 +6,13 @@ from .metrics import (
     metrics_middleware,
 )
 
+from .tracing import (
+    tracing_middleware,
+)
+
 
 __all__ = [
     "request_logging_middleware",
     "metrics_middleware",
+    "tracing_middleware",
 ]
