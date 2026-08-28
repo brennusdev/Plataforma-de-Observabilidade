3f78db7c98d5ac586979d4b9700d75d595f34626
Brenno Pereira <brennuscontato@gmail.com>
2026-08-28 19:07:30 -0300

    Create __init__.py
    Atualização v11 do projeto

---
+++ observability-platform/app/intelligence/__init__.py
@@ -0,0 +1,11 @@
+"""
+Incident Intelligence Engine.
+
+Responsável por:
+
+- correlação de sinais;
+- detecção de incidentes;
+- análise de causa provável;
+- cálculo de confiança;
+- geração de recomendações.
+"""
